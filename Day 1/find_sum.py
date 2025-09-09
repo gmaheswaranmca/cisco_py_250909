0c9d10ee341f6bfbfc9f2f59387265f309b875f9
gmaheswaranmca <gmaheswaranmca@gmail.com>
2025-09-09 15:33:27 +0530

    Started Python Learning

---
+++ Day 1/find_sum.py
@@ -0,0 +1,16 @@
+# sum of two numbers
+# author : Yashaswini
+def find_sum(first, second):    
+    result = first + second 
+    return result 
+
+# 99 users
+# user: Bhanu 
+res = find_sum(10.5, 20.5)
+# store the result in file 
+print(res)
+
+#user: Bhaskar 
+res2 = find_sum(5, 6)
+# save into db
+print(res2) 
